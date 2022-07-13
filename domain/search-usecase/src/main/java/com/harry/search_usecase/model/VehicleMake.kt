70960f6bcc284e485b33e5bc5fbe4c7e7cea9990
Harry <hurdysteam@gmail.com>
2022-07-14 00:22:02 +0100

    remove unused function

--- domain/search-usecase/src/main/java/com/harry/search_usecase/model/VehicleMake.kt
+++ domain/search-usecase/src/main/java/com/harry/search_usecase/model/VehicleMake.kt
@@ -1,11 +1,3 @@
 package com.harry.search_usecase.model
 
-import com.harry.make_and_model_repository.model.Make
-
-data class VehicleMake(val name: String, val models: List<String>) {
-    companion object {
-        internal fun fromMake(make: Make): VehicleMake {
-            return VehicleMake(make.name, make.models)
-        }
-    }
-}
+data class VehicleMake(val name: String, val models: List<String>)
